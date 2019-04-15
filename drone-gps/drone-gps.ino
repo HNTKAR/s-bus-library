f565526d2e41c9a20f3bf6ae393f0b437bf35bdb
kusari-k <s15426@salesio-sp.ac.jp>
2019-04-16 04:36:15 +0900

    gps-lib

---
+++ drone-gps/drone-gps.ino
@@ -0,0 +1,78 @@
+#include "A2CanDecoderLib.h"
+#include "FlexCAN.h"
+#include "FastCRC.h"
+
+// Use Serial to output data via USB or Serial1 to use Teensy's first serial port
+#define SERIAL_PORT Serial
+#define NSPIN 8//lat
+#define WEPIN 9//lon
+#define DOWNPIN 10//kakou
+
+uint32_t currTime, otherTime, clockTime;
+char dateTime[20];
+uint32_t messageId;
+float latpm, lonpm, latgopoint, longopoint;
+int latp, latm, lonp, lonm, latadd, lonadd, addspoint;
+void setup()
+{
+  SERIAL_PORT.begin(115200);
+  A2CanDecoder.begin();
+  pinMode(NSPIN, OUTPUT);
+  pinMode(WEPIN, OUTPUT);
+  pinMode(DOWNPIN, OUTPUT);
+  pinMode(13, OUTPUT);
+  latp = 0;
+  latm = 0;
+  lonp = 0;
+  lonm = 0;
+  latadd = 0;
+  lonadd = 0;
+  latgopoint = 35.605897;
+  longopoint = 139.358482;
+  addspoint = 0;
+  digitalWrite(13, HIGH);
+}
+
+void loop() {
+  messageId = A2CanDecoder.decode();
+  currTime = millis();
+
+  if (otherTime < currTime)
+  {
+    otherTime = currTime + 200;
+    SERIAL_PORT.print("Lat: "); SERIAL_PORT.print(A2CanDecoder.getLat(), 7);
+    SERIAL_PORT.print(", Lon: "); SERIAL_PORT.print(A2CanDecoder.getLon(), 7);
+
+    latpm = latgopoint - A2CanDecoder.getLat(); //latpmが+なら北
+    lonpm = longopoint - A2CanDecoder.getLon(); //lonpmが+なら東
+    if (latpm >= 0) {
+      digitalWrite(NSPIN, HIGH);
+      SERIAL_PORT.println("NS HIGH");
+      latp = 1;
+
+    } else {
+      digitalWrite(NSPIN, LOW);
+      SERIAL_PORT.println("NS LOW");
+      latm = 1;
+    }
+    if (lonpm >= 0) {
+      digitalWrite(WEPIN, HIGH);
+      SERIAL_PORT.println("WE HIGH");
+      lonp = 1;
+    }
+    else {
+      digitalWrite(WEPIN, LOW);
+      SERIAL_PORT.println("WE LOW");
+      lonm = 1;
+    }
+    latadd = latp + latm;
+    lonadd = lonp + lonm;
+    addspoint = latadd + lonadd;
+    if (addspoint == 4) {
+      digitalWrite(DOWNPIN, HIGH);
+      SERIAL_PORT.println("DOWN HIGH");
+      digitalWrite(13, LOW);
+    }
+  }
+}
+
---
+++ latlongpsserialtest/latlongpsserialtest.ino
@@ -0,0 +1,89 @@
+#define NSPIN 9
+#define WEPIN 10
+#define DOWNPIN 11
+
+float latpm, lonpm, latgopoint, longopoint, incomingByte, latnpoint, lonnpoint;
+int latp, latm, lonp, lonm, latadd, lonadd, addspoint, ded;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(NSPIN, OUTPUT);
+  pinMode(WEPIN, OUTPUT);
+  pinMode(DOWNPIN, OUTPUT);
+  ded = 0;
+  latp = 0;
+  latm = 0;
+  lonp = 0;
+  lonm = 0;
+  latadd = 0;
+  lonadd = 0;
+  latgopoint = 35.606386;
+  longopoint = 139.358479;
+  addspoint = 0;
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read() - 0x30;
+
+    Serial.println(incomingByte);
+    Serial.println(ded);
+    ded = ded + 1;
+    if (ded == 1) {
+      latnpoint = incomingByte * 10;
+      Serial.println(latnpoint);
+      Serial.println("lat10 ok");
+    }
+    else if (ded == 2) {
+      latnpoint = incomingByte + latnpoint;
+      Serial.println(latnpoint);
+      Serial.println("lat ok");
+    }
+    if (ded == 3) {
+      lonnpoint = incomingByte * 100;
+      Serial.println(lonnpoint);
+      Serial.println("lon100 ok");
+    }
+    else if (ded == 4) {
+      lonnpoint = incomingByte * 10 + lonnpoint;
+      Serial.println(lonnpoint);
+      Serial.println("lon10 ok");
+    }
+    else if (ded == 5) {
+      lonnpoint = incomingByte + lonnpoint;
+      Serial.println(lonnpoint);
+      Serial.println("lon ok");
+      ded = 0;
+      latadd = latp + latm;
+      lonadd = lonp + lonm;
+      addspoint = latadd + lonadd;
+
+
+      latpm = latgopoint - latnpoint; //latpmが+なら北
+      lonpm = longopoint - lonnpoint; //lonpmが+なら東
+      if (latpm >= 0) {
+        digitalWrite(NSPIN, HIGH);
+        latp = 1;
+      } else {
+        digitalWrite(NSPIN, LOW);
+        latm = 1;
+      }
+      if (lonpm >= 0) {
+        digitalWrite(WEPIN, HIGH);
+        lonp = 1;
+      }
+      else {
+        digitalWrite(WEPIN, LOW);
+        lonm = 1;
+      }
+    }
+      latadd = latp + latm;
+      lonadd = lonp + lonm;
+      addspoint = latadd + lonadd;
+    if (addspoint == 4) {
+      digitalWrite(DOWNPIN, HIGH);
+    }
+    Serial.println("Done");
+  }
+}
